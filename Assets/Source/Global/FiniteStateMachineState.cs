135a693cb4da44dd4aa57339750a95abf53a3147
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-24 18:48:52 +0300

    Create FSM

---
+++ Assets/Source/Global/FiniteStateMachineState.cs
@@ -0,0 +1,27 @@
+﻿public abstract class FiniteStateMachineState<T>
+    where T : FiniteStateMachineState<T>
+{
+    private readonly FiniteStateMachine<T> Machine;
+
+    public FiniteStateMachineState(FiniteStateMachine<T> machine)
+    {
+        Machine = machine;
+    }
+
+    public void SetState<TU>()
+    {
+        Machine.SetState(typeof(TU));
+    }
+
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void Update()
+    {
+    }
+}
