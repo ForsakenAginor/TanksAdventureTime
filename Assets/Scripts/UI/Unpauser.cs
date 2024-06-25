3aadc655bd52c7358323fe29578556b2b9dc253e
ForsakenAginor <juriys2022@mail.ru>
2024-06-25 19:30:32 +0400

    add battle scene interface

---
+++ Assets/Scripts/UI/Unpauser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Unpauser : MonoBehaviour
+{
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        Time.timeScale = 1f;
+    }
+}
