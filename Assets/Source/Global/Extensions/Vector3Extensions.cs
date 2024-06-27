251e633abcaa27bc782a807ed004c1ffbfbdbe06
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-27 21:29:03 +0300

    Vector3 extensions

---
+++ Assets/Source/Global/Extensions/Vector3Extensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Vector3Extensions
+{
+    public static Vector3 CalculateVelocity(this Vector3 forward, Vector3 direction, float angleRadian, float gravity)
+    {
+        float y = direction.y;
+        float x = new Vector3(direction.x, (float)ValueConstants.Zero, direction.z).magnitude;
+        return forward * Mathf.Sqrt(
+            Mathf.Abs(
+                gravity * x * x /
+                ((float)ValueConstants.Two * (y - Mathf.Tan(angleRadian) * x) * Mathf.Cos(angleRadian) * Mathf.Cos(angleRadian)))
+        );
+    }
+
+    public static Vector3 RotateAlongY(this Vector3 forward, Vector3 direction)
+    {
+        Vector3 newForward = Vector3.ClampMagnitude(
+            new Vector3(direction.x, (float)ValueConstants.Zero, direction.z),
+            new Vector3(forward.x, (float)ValueConstants.Zero, forward.z).magnitude);
+        newForward.y = forward.y;
+        return newForward;
+    }
+}
