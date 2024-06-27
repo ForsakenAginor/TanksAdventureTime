cba52eb2626afbcad5785b0d942e27e31b099c39
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-28 01:06:44 +0300

    Update EnemyThinker.cs

--- Assets/Source/Enemies/Logic/EnemyThinker.cs
+++ Assets/Source/Enemies/Logic/EnemyThinker.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Random = UnityEngine.Random;
 
 namespace Enemies
 {
     public class EnemyThinker
     {
+        private const float MinDelay = 0f;
+        private const float MaxDelay = 0.5f;
+
         private readonly CancellationToken Token;
         private readonly TimeSpan Time;
 
         public EnemyThinker(CancellationToken token, float delay)
         {
             Token = token;
-            Time = TimeSpan.FromSeconds(delay);
+            Time = TimeSpan.FromSeconds(delay + Random.Range(MinDelay, MaxDelay));
         }
 
         public event Action Updated;
