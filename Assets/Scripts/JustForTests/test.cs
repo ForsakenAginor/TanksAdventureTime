e3f44fc761fd0be68cfce14c6ed85ae7c8b24aa0
ForsakenAginor <juriys2022@mail.ru>
2024-06-30 19:11:09 +0400

    fix aim direction

--- Assets/Scripts/JustForTests/test.cs
+++ Assets/Scripts/JustForTests/test.cs
@@ -8,6 +8,7 @@ public class test : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private TextMeshProUGUI _text1;
     [SerializeField] private TextMeshProUGUI _text2;
+    [SerializeField] private TextMeshProUGUI _text3;
     [SerializeField] private GameObject _cannon;
     [SerializeField] private PidRegulator _pidController = new();
     [SerializeField] private float _aimingSpeed;
@@ -22,7 +23,7 @@ public class test : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_playerInput.ReadRotation() != Vector2.zero)
+        if (_playerInput.ReadRotation() != Vector2.zero )
         {
             Vector3 temp = _playerInput.ReadRotation().normalized;
             _text.text = temp.ToString();
@@ -30,13 +31,13 @@ public class test : MonoBehaviour
             return;
         }
 
-        Vector3 forward = Vector3.forward;
-
         float y = _cannon.transform.localRotation.eulerAngles.y;
         float currentAngle = y <= 180f ? y : y - 360f;
         _text1.text = currentAngle.ToString();
-        float requiredAngle = Vector3.SignedAngle(Vector3.forward, _aimDirection, Vector3.up);
-        _text2.text = requiredAngle.ToString();
+        Vector3 currentPosition = new Vector3(Mathf.Sin(y * Mathf.Deg2Rad),0, Mathf.Cos(y * Mathf.Deg2Rad));
+        _text2.text = currentPosition.ToString();
+        float requiredAngle = currentAngle + Vector3.SignedAngle(currentPosition, _aimDirection, Vector3.up);
+        _text3.text = requiredAngle.ToString();
 
         /*
         _text.text = _aimDirection.ToString();
