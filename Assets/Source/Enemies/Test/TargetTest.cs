6de0cf4a1b049ad8802171133a6686712e3c7858
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-03 04:10:40 +0300

    The test scene was rebuilt

--- Assets/Source/Enemies/Test/TargetTest.cs
+++ Assets/Source/Enemies/Test/TargetTest.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Enemies
@@ -5,20 +7,25 @@ namespace Enemies
     [RequireComponent(typeof(Collider))]
     public class TargetTest : MonoBehaviour, IPlayerTarget
     {
+        private const float MinValue = (float)ValueConstants.Zero;
+
         [SerializeField] private Transform _transform;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        [SerializeField] private float _shakeDuration = 1f;
+        [SerializeField] private float _shakeAmplitude = 1f;
+        [SerializeField] private float _shakeFrequency = 1f;
 
         private Collider _collider;
+        private float _timer = 0f;
+        private CinemachineBasicMultiChannelPerlin _cameraNoise;
 
         public Vector3 Position => _transform.position;
 
         private void Awake()
         {
             _collider = GetComponent<Collider>();
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            print("Target Hit");
+            _cameraNoise = _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            _cameraNoise.m_FrequencyGain = _shakeFrequency;
         }
 
         public Vector3 GetClosestPoint(Vector3 position)
@@ -28,6 +35,25 @@ namespace Enemies
 
         public void TakeHit(HitTypes type)
         {
+            if (type != HitTypes.Explosion)
+                return;
+
+            Shake().Forget();
+        }
+
+        private async UniTaskVoid Shake()
+        {
+            _timer = _shakeDuration;
+
+            while (_timer > MinValue)
+            {
+                _cameraNoise.m_AmplitudeGain = _shakeAmplitude;
+                _timer -= Time.deltaTime;
+                await UniTask.NextFrame(destroyCancellationToken);
+            }
+
+            _cameraNoise.m_AmplitudeGain = MinValue;
+            _timer = MinValue;
         }
     }
 }
