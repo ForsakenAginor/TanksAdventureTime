6ad998982c8f7d874f591a5670d8c73ab9d495da
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-07 17:45:21 +0300

    New scripts have been created

---
+++ Assets/Source/Enemies/EnemyCollision.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyCollision : MonoBehaviour, IDamageableTarget
+    {
+        private Transform _transform;
+
+        public event Action<HitTypes> HitTook;
+
+        public Vector3 Position => _transform.position;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.TryGetComponent(out IPermanentKiller _) == false)
+                return;
+
+            TakeHit(HitTypes.PermanentDeath);
+        }
+
+        public void Init(Transform transform)
+        {
+            _transform = transform;
+        }
+
+        public void TakeHit(HitTypes type)
+        {
+            HitTook?.Invoke(type);
+        }
+    }
+}
