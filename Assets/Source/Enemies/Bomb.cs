9a42aea4a2dab1a057f46f4e8cbdca9c93ce86d6
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-07 18:01:43 +0300

    Update Bomb.cs

--- Assets/Source/Enemies/Bomb.cs
+++ Assets/Source/Enemies/Bomb.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Enemies
 {
     public class Bomb : MonoBehaviour
     {
         [SerializeField] private float _explosionRadius;
+        [SerializeField] private AudioSource _sound;
+        [SerializeField] private ParticleSystem _particle;
+        [SerializeField] private GameObject _colliderBody;
 
         private IExplosive _explosive;
         private Transform _transform;
+        private bool _didExplode;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _explosionRadius);
+        }
 
         private void OnCollisionEnter()
         {
@@ -22,12 +33,29 @@ namespace Enemies
 
         public void Explode()
         {
+            if (_didExplode == true)
+                return;
+
+            _didExplode = true;
+            _colliderBody.SetActive(false);
             _explosive.Explode(_transform.position, _explosionRadius);
+            OnExploded().Forget();
         }
 
         public virtual void OnExploding()
         {
             Explode();
+        }
+
+        private async UniTaskVoid OnExploded()
+        {
+            _sound.Play();
+            _particle.Play();
+
+            while (_particle.isPlaying || _sound.isPlaying)
+                await UniTask.NextFrame(destroyCancellationToken);
+
+            Destroy(gameObject);
         }
     }
 }
