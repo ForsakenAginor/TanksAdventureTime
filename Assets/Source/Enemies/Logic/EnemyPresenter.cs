e7159407a6402a28225961d019fad4fbe4e0bb54
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-07 18:00:15 +0300

    Update EnemyPresenter.cs

--- Assets/Source/Enemies/Logic/EnemyPresenter.cs
+++ Assets/Source/Enemies/Logic/EnemyPresenter.cs
@@ -4,16 +4,33 @@
     {
         private readonly FiniteStateMachine<EnemyState> Machine;
         private readonly EnemyThinker Thinker;
+        private readonly EnemyCollision Collision;
+        private readonly IDamageable Health;
+        private readonly HitConfiguration HitConfiguration;
+        private readonly EnemyDeathEffect Death;
 
-        public EnemyPresenter(FiniteStateMachine<EnemyState> machine, EnemyThinker thinker)
+        public EnemyPresenter(
+            FiniteStateMachine<EnemyState> machine,
+            EnemyThinker thinker,
+            EnemyCollision collision,
+            IDamageable health,
+            HitConfiguration hitConfiguration,
+            EnemyDeathEffect death)
         {
             Machine = machine;
             Thinker = thinker;
+            Collision = collision;
+            Health = health;
+            HitConfiguration = hitConfiguration;
+            Death = death;
         }
 
         public void Enable()
         {
             Thinker.Updated += OnUpdated;
+            Collision.HitTook += OnHitTook;
+            Health.Died += OnDied;
+            Health.DamageTook += OnDamageTook;
 
             Thinker.Start();
         }
@@ -21,13 +38,37 @@
         public void Disable()
         {
             Thinker.Updated -= OnUpdated;
+            Collision.HitTook -= OnHitTook;
+            Health.Died -= OnDied;
+            Health.DamageTook -= OnDamageTook;
 
-            Machine.Exit();
+            OnDisable();
         }
 
         private void OnUpdated()
         {
             Machine.Update();
+        }
+
+        private void OnHitTook(HitTypes type)
+        {
+            Health.TakeDamage(HitConfiguration.GetDamage(type));
+        }
+
+        private void OnDied()
+        {
+            OnDisable();
+            Death.Die();
+        }
+
+        private void OnDamageTook(int value)
+        {
+        }
+
+        private void OnDisable()
+        {
+            Thinker.Stop();
+            Machine.Exit();
         }
     }
 }
