932c21c1d13ca6a94d6a17205b239fa585a7bc87
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-08 19:46:12 +0300

    A weapon has been created for the player

---
+++ Assets/Source/Player/Weapons/TestWeaponSetup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Projectiles;
+using UnityEngine;
+
+public class TestWeaponSetup : MonoBehaviour
+{
+    [Header("Factory")] [SerializeField]
+    private SpawnableProjectile _projectile;
+
+    [SerializeField] private HitEffect _hitEffect;
+    [SerializeField] private float _attackAngle;
+
+    [Header("Weapon")] [SerializeField]
+    private Transform _shootPoint;
+
+    [SerializeField] private Transform _transform;
+    [SerializeField] private LineRenderer _line;
+    [SerializeField] private float _maxDistance;
+
+    private PlayerWeapon _weapon;
+
+    private void Awake()
+    {
+        _weapon = new PlayerWeapon(
+            new PlayerProjectileFactory(
+                _projectile,
+                _hitEffect,
+                new OverlapExplosive(),
+                _attackAngle * Mathf.Deg2Rad),
+            _shootPoint,
+            _transform,
+            _line,
+            _maxDistance,
+            _attackAngle * Mathf.Deg2Rad);
+    }
+
+    private void OnDestroy()
+    {
+        _weapon.StopDrawTrajectory();
+    }
+
+    private void Update()
+    {
+        _weapon.DrawTrajectory();
+    }
+}
---
+++ Assets/Source/Projectiles/OverlapExplosive.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Projectiles
+{
+    public class OverlapExplosive : IExplosive
+    {
+        private readonly LayerMask _mask;
+        
+        public void Explode(Vector3 position, float radius)
+        {
+            Physics.OverlapSphere(position, radius);
+        }
+    }
+}
