70773c7d04bf80cde93e2a81bd41b5eb277ea095
ForsakenAginor <juriys2022@mail.ru>
2024-07-10 03:54:50 +0400

    add dinamic adding audio sources to mixer

--- Assets/Source/Sound/AudioMixer/SoundInitializer.cs
+++ Assets/Source/Sound/AudioMixer/SoundInitializer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +10,20 @@ namespace Assets.Source.Sound.AudioMixer
         private readonly AudioData _audioData = new ();
 
         [Header("AudioSources")]
-        [SerializeField] private AudioSource[] _allSources;
-        [SerializeField] private AudioSource[] _effectsSources;
-        [SerializeField] private AudioSource[] _musicSources;
+        [SerializeField] private List<AudioSource> _allSources;
+        [SerializeField] private List<AudioSource> _effectsSources;
+        [SerializeField] private List<AudioSource> _musicSources;
 
         [Header("Sliders")]
         [SerializeField] private Slider _masterVolumeSlider;
         [SerializeField] private Slider _effectsVolumeSlider;
         [SerializeField] private Slider _musicVolumeSlider;
+
+        [Header("VolumeChangers")]
+        private VolumeChanger _masterChanger;
+        private VolumeChanger _effectsChanger;
+        private VolumeChanger _musicChanger;
+
 
         private void Start()
         {
@@ -23,12 +31,12 @@ namespace Assets.Source.Sound.AudioMixer
             _effectsVolumeSlider.value = _audioData.GetEffectsVolume();
             _musicVolumeSlider.value = _audioData.GetMusicVolume();
 
-            VolumeChanger masterChanger = new (_allSources, _audioData.GetMasterVolume());
-            VolumeChanger effectsChanger = new (_effectsSources, _audioData.GetEffectsVolume());
-            VolumeChanger musicChanger = new (_musicSources, _audioData.GetMusicVolume());
-            VolumeChangeView masterChangerView = new (masterChanger, _masterVolumeSlider);
-            VolumeChangeView effectsChangerView = new (effectsChanger, _effectsVolumeSlider);
-            VolumeChangeView musicChangerView = new (musicChanger, _musicVolumeSlider);
+            _masterChanger = new (_allSources, _audioData.GetMasterVolume());
+            _effectsChanger = new (_effectsSources, _audioData.GetEffectsVolume());
+            _musicChanger = new (_musicSources, _audioData.GetMusicVolume());
+            VolumeChangeView masterChangerView = new (_masterChanger, _masterVolumeSlider);
+            VolumeChangeView effectsChangerView = new (_effectsChanger, _effectsVolumeSlider);
+            VolumeChangeView musicChangerView = new (_musicChanger, _musicVolumeSlider);
         }
 
         public void SaveSettings()
@@ -36,6 +44,24 @@ namespace Assets.Source.Sound.AudioMixer
             _audioData.SaveMasterVolume(_masterVolumeSlider.value);
             _audioData.SaveEffectsVolume(_effectsVolumeSlider.value);
             _audioData.SaveMusicVolume(_musicVolumeSlider.value);
+        }
+
+        public void AddMusicSource(AudioSource music)
+        {
+            if(music == null)
+                throw new ArgumentNullException(nameof(music));
+
+            _masterChanger.AddAudioSource(music);
+            _musicChanger.AddAudioSource(music);
+        }
+
+        public void AddEffectSource(AudioSource effect)
+        {
+            if (effect == null)
+                throw new ArgumentNullException(nameof(effect));
+
+            _masterChanger.AddAudioSource(effect);
+            _effectsChanger.AddAudioSource(effect);
         }
     }
 }
