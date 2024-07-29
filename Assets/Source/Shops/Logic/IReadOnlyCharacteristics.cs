2cb4e53067acc51ed5dcde1398f45e6624d29b0a
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-29 16:22:27 +0300

    Added basic logic of shop

---
+++ Assets/Source/Shops/Logic/IReadOnlyCharacteristics.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Shops
+{
+    public interface IReadOnlyCharacteristics<T>
+    {
+        public IReadOnlyList<SerializedPair<GoodNames, T>> Content { get; }
+    }
+}
---
+++ Assets/Source/Shops/Logic/Purchases.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shops
+{
+    [Serializable]
+    public class Purchases<T> : IReadOnlyCharacteristics<T>
+    {
+        public List<SerializedPair<GoodNames, T>> Objects;
+        public Purchases(List<SerializedPair<GoodNames, T>> stored)
+        {
+            Objects = stored;
+        }
+
+        public IReadOnlyList<SerializedPair<GoodNames, T>> Content => Objects;
+    }
+}
