bc08771147079348e5dc070da38681a744e4b17e
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-07-31 11:56:54 +0300

    Fix addcomponent

--- Assets/Source/DestructionObject/Buncer/Bunker.cs
+++ Assets/Source/DestructionObject/Buncer/Bunker.cs
@@ -6,20 +6,20 @@ public class Bunker : MonoBehaviour, IDamageable
 {
     [SerializeField] private List<BunkerPart> _bunkerDetails;
 
-    public event Action<float> TookDamage;
-    public event Action Died;
-
     private int _minValue = 0;
     private float _maxHeath = 100;
     private float _currentHealth;
     private float _damage;
 
-    private void Awake() => Init();
+    public event Action<float> TookDamage;
+    public event Action Died;
 
-    private void Init()
+    public Bunker Init(float maxHealth)
     {
+        _maxHeath = maxHealth;
         _currentHealth = _maxHeath;
         _damage = Mathf.CeilToInt(_currentHealth / _bunkerDetails.Count);
+        return this;
     }
 
     public void TakeDamage(int value)
